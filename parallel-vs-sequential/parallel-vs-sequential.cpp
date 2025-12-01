1a4dc8a380a78ce242d20b512731aa8928a8c059
Ruslan <gafarov-ruslan@internet.ru>
2025-12-02 01:32:01 +0300

    Реализованы генерация псевдослучайного индекса и поиск повторяющихся значений по индексу.

--- parallel-vs-sequential/parallel-vs-sequential.cpp
+++ parallel-vs-sequential/parallel-vs-sequential.cpp
@@ -1,23 +1,29 @@
 ﻿#include <iostream>
 using namespace std;
 
-void generateArray(int arr[], int size)
-{
-	for (int i = 0; i < size; i++) {
-		arr[i] = rand() % 100;
-		cout << arr[i] << " ";
-	}
-		
-	
-}
+
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 	const int N = 100000;
 	int arr[N];
+	int count = 0;
 
-	generateArray(arr, N);
+	//заполняем массив случайными числами от 0 до 100
+	for (int i = 0; i < N; i++)
+		arr[i] = rand() % 101;
+
+	int value = rand() % 100001; //присваиваем случайный индекс
+	int temp = arr[value];
+
+	//ищем поввторяющиеся значения в массиве
+	for (int i = 0; i < N; i++)
+	{
+		if(arr[i] == temp)
+			count++;
+	}
+	cout << count;
 
 }
 
